45aa40f2b7a2da7951b9b05452eeb9b031f4414b
AndrewOlson24 <andy.w.olson@gmail.com>
2023-06-27 21:53:34 -0600

    dyuoipo[]\

--- Chambers/scripts/today.js
+++ Chambers/scripts/today.js
@@ -89,6 +89,30 @@ document.querySelector("#date2").textContent = fullydate;
 
 
 
+
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+
+
                     
 
 
--- Chambers/scripts/update.js
+++ Chambers/scripts/update.js
@@ -72,17 +72,4 @@ images.forEach(image => {
 
 
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("grid");
-
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", () => {
-	display.classList.remove("grid");
-	display.classList.add("list");
-});
 
