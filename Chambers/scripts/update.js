44d21ae921cd76eec9407dc4120dce34eddad81c
AndrewOlson24 <andy.w.olson@gmail.com>
2023-07-01 17:31:28 -0600

    asdf

--- Chambers/scripts/update.js
+++ Chambers/scripts/update.js
@@ -77,3 +77,52 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+
+
+
+
+  const url = 'https://andrewolson24.github.io/wdd230/Chambers/scripts/data.json';
+  getProphetData();
+
+  async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  //console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+  displayProphets(data.prophets);
+  }
+
+  const displayProphets = (prophets) => {
+  const cards = document.querySelector('div.cards'); // select the output container element
+
+  prophets.forEach((prophet) => {
+  // Create elements to add to the div.cards element
+  let card = document.createElement('section');
+  let h2 = document.createElement('h2');
+  let birthdate = document.createElement('p');
+  let portrait = document.createElement('img');
+  let birthplace = document.createElement('p');
+
+  // Build the h2 content out to show the prophet's full name - finish the template string
+  h2.textContent = `${prophet.name} ${prophet.lastname}`;
+  birthdate.innerHTML = `<strong>Date of Birth</stong>: ${prophet.birthdate}`;
+  birthplace.innerHTML = `<strong>place of Birth</stong>: ${prophet.birthplace}`;
+  // Build the image portrait by setting all the relevant attribute
+  portrait.setAttribute('src', prophet.imageurl);
+  portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+  portrait.setAttribute('loading', 'lazy');
+  portrait.setAttribute('width', '340');
+  portrait.setAttribute('height', '440');
+
+  // Append the section(card) with the created elements
+  card.appendChild(h2);
+  card.appendChild(birthdate);
+  card.appendChild(birthplace);
+  card.appendChild(portrait);
+
+  cards.appendChild(card);
+  }); // end of forEach loop
+} // end of function expression
+
+
+
+
