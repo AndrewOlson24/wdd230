1e40dfe23514eb564a12bb130733df5d7a6f38fa
AndrewOlson24 <andy.w.olson@gmail.com>
2023-07-01 13:49:36 -0600

    lets gooooo

--- Chambers/scripts/update.js
+++ Chambers/scripts/update.js
@@ -68,7 +68,11 @@ images.forEach(image => {
 
 
 
-
+document.addEventListener("DOMContentLoaded", function() {
+	var timestampField = document.getElementById("timestamp");
+	var currentTimestamp = new Date().toISOString();
+	timestampField.value = currentTimestamp;
+  });
 
 
 
