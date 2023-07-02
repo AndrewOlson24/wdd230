913406b418db9e78cdbca6d1c7c20af0b4da19e9
AndrewOlson24 <andy.w.olson@gmail.com>
2023-07-01 21:10:18 -0600

    asdfsdf

--- Chambers/scripts/update.js
+++ Chambers/scripts/update.js
@@ -81,47 +81,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-  const url = 'https://andrewolson24.github.io/wdd230/Chambers/scripts/data.json';
-  getProphetData();
+  fetch('data.json').then(response => response.json())
+                .then(data => {
+                    // Process the JSON data
+                    displayData(data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
 
-  async function getProphetData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  //console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-  displayProphets(data.prophets);
-  }
+                // Function to display the data in the HTML
+                function displayData(data) {
+                const dataContainer = document.getElementById('dataContainer');
 
-  const displayProphets = (prophets) => {
-  const cards = document.querySelector('div.cards'); // select the output container element
+                // Iterate over the array and extract the desired information
+                data.forEach(item => {
+                    const title = item.title;
+                    const numofchildren = item.numofchildren;
+                    const imageUrl = item.image_url;
 
-  prophets.forEach((prophet) => {
-  // Create elements to add to the div.cards element
-  let card = document.createElement('section');
-  let h2 = document.createElement('h2');
-  let birthdate = document.createElement('p');
-  let portrait = document.createElement('img');
-  let birthplace = document.createElement('p');
+                    // Create HTML elements to display the extracted information
+                    const itemDiv = document.createElement('div');
+                    const titleElement = document.createElement('h3');
+                    const dateElement = document.createElement('p');
+                    const imageElement = document.createElement('img');
 
-  // Build the h2 content out to show the prophet's full name - finish the template string
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  birthdate.innerHTML = `<strong>Date of Birth</stong>: ${prophet.birthdate}`;
-  birthplace.innerHTML = `<strong>place of Birth</stong>: ${prophet.birthplace}`;
-  // Build the image portrait by setting all the relevant attribute
-  portrait.setAttribute('src', prophet.imageurl);
-  portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-  portrait.setAttribute('loading', 'lazy');
-  portrait.setAttribute('width', '340');
-  portrait.setAttribute('height', '440');
+                    titleElement.textContent = title;
+                    dateElement.textContent = `Date Established: ${dateEstablished}`;
+                    imageElement.src = imageUrl;
 
-  // Append the section(card) with the created elements
-  card.appendChild(h2);
-  card.appendChild(birthdate);
-  card.appendChild(birthplace);
-  card.appendChild(portrait);
+                    itemDiv.appendChild(titleElement);
+                    itemDiv.appendChild(dateElement);
+                    itemDiv.appendChild(imageElement);
 
-  cards.appendChild(card);
-  }); // end of forEach loop
-} // end of function expression
+                    dataContainer.appendChild(itemDiv);
+                });
+                }
 
 
 
